b82f3a8087ef30ecde98d7d38cf0c5f9f67c5a1a
julianrezmasud <julianrezmasud84@gmail.com>
2023-09-27 19:15:29 -0400

    Fetch

--- main.js
+++ main.js
@@ -1,28 +1,31 @@
-fetch("products.json")
-    .then((data) => {
-        console.log(data.json())
-    });
-fetch("products.json").then(response => response.json()).then(data => console.log(data))
-
-
-const categories = [...new Set(product.map((item) => { return item }))]
-let i = 0;
-document.getElementById('container2').innerHTML = categories.map((item) => {
-    let { img, name, price } = item;
-    return (
-        `<div class='box'>
-            <div class='img-box'>
-                <img class='images' src=${img}></img>
-            </div>
-        <div class='bottom'>
-        <p>${name}</p>
-        <h2>$ ${price}.00</h2>` +
-        "<button onclick='addtocart(" + (i++) + ")'>Add to cart</button>" +
-        `</div>
-        </div>`
-    )
-}).join('')
+let categories = []
 
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 displaycart()
+
+fetch("products.json")
+    .then(response => response.json())
+    .then(data => {
+        categories = data
+        let i = 0;
+        document.getElementById('container2').innerHTML = categories.map((item) => {
+            let { img, name, price } = item;
+            return (
+                `<div class='box'>
+                <div class='img-box'>
+                    <img class='images' src=${img}></img>
+                </div>
+            <div class='bottom'>
+            <p>${name}</p>
+            <h2>$ ${price}.00</h2>` +
+                "<button onclick='addtocart(" + (i++) + ")'>Add to cart</button>" +
+                `</div>
+            </div>`
+            )
+        }).join('')
+
+
+    });
+
+
